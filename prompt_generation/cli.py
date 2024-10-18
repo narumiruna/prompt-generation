c985089194424bad37dff62a95b1285742720884
narumi <toucans-cutouts0f@icloud.com>
2024-10-18 14:44:07 +0800

    add prompt generation script

--- prompt_generation/cli.py
+++ prompt_generation/cli.py
@@ -1,2 +1,30 @@
-def main():
-    print("Hello, World!")
+import gradio as gr
+from dotenv import find_dotenv
+from dotenv import load_dotenv
+from loguru import logger
+
+from .openai import create_completion
+from .prompt import META_PROMPT
+from .utils import save_text
+
+
+def generate_response(message, history):
+    messages = [{"role": "system", "content": META_PROMPT}]
+    messages += history
+    messages += [{"role": "user", "content": message}]
+
+    logger.info("messages: {}", messages)
+
+    try:
+        response = create_completion(messages)
+        logger.info("response: {}", response)
+        save_text(response, "prompt.txt")
+        return response
+    except Exception as e:
+        logger.error("unable to generate response: {}", e)
+        return str(e)
+
+
+def main() -> None:
+    load_dotenv(find_dotenv())
+    gr.ChatInterface(generate_response, type="messages").launch()
---
+++ prompt_generation/utils.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+
+def load_text(f: str | Path) -> str:
+    with open(f) as fp:
+        return fp.read()
+
+
+def save_text(text: str, f: str | Path) -> None:
+    with open(f, "w") as fp:
+        return fp.write(text)
